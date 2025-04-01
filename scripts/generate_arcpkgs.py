c0f4be8a9b60d1b221187cb5b059f78027235c47
freeze-dolphin <freeze-dolphin@outlook.com>
2025-04-01 22:56:39 +0800

    bump Etoile version

--- scripts/generate_arcpkgs.py
+++ scripts/generate_arcpkgs.py
@@ -4,8 +4,8 @@ from urllib.request import urlretrieve
 import zipfile
 
 if __name__ == "__main__":
-    etoile_release = "v0.2.0"
-    etoile_version = "EtoileResurrection-e7e3357"
+    etoile_release = "v1`.0.0"
+    etoile_version = "EtoileResurrection-7ced5d9"
     etoile_zip_file = "EtoileResurrection.zip"
     urlretrieve(
         f"https://github.com/freeze-dolphin/EtoileResurrection/releases/download/{etoile_release}/{etoile_version}.zip",
