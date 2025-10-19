f9756004359fb6c91e9225f7fe610480f5cfd81a
freeze-dolphin <freeze-dolphin@outlook.com>
2025-10-19 15:28:53 +0800

    update Etoile version, build combined packages

--- scripts/generate_arcpkgs.py
+++ scripts/generate_arcpkgs.py
@@ -4,8 +4,8 @@ from urllib.request import urlretrieve
 import zipfile
 
 if __name__ == "__main__":
-    etoile_release = "v1.0.5"
-    etoile_version = "EtoileResurrection-ebc8f8b"
+    etoile_release = "v1.0.8"
+    etoile_version = "EtoileResurrection-1b6e21c"
     etoile_zip_file = "EtoileResurrection.zip"
     urlretrieve(
         f"https://github.com/freeze-dolphin/EtoileResurrection/releases/download/{etoile_release}/{etoile_version}.zip",
@@ -20,19 +20,35 @@ if __name__ == "__main__":
 
     print("EtoileResurrection Extracted")
 
-    os.mkdir("arcpkgs")
+    paths = ["arcpkgs", "arcpkgs/combine", "arcpkgs/aprilfools", "arcpkgs/aprilfools/combine"]
+
+    for path in paths:
+        if not os.path.exists(path):
+            os.mkdir(path)
 
     if sys.platform.startswith("win"):
         os.system(
             f"scripts\\{etoile_version}\\bin\\EtoileResurrection pack fragments-category\\songs\\songlist --songId=.* -re --prefix=lowiro -o arcpkgs"
         )
         os.system(
-            f"scripts\\{etoile_version}\\bin\\EtoileResurrection pack fragments-category\\songs\\songlist_aprilfools --songId=.* -re --prefix=lowiro -o arcpkgs"
+            f"scripts\\{etoile_version}\\bin\\EtoileResurrection pack fragments-category\\songs\\songlist_aprilfools --songId=.* -re --prefix=lowiro -o arcpkgs\\aprilfools"
+        )
+        os.system(
+            f"scripts\\{etoile_version}\\bin\\EtoileResurrection combine --prefix=lowiro -o arcpkgs\\combine -s.* -re arcpkgs\\*.arcpkg fragments-category\\songs\\songlist fragments-category\\songs\\packlist"
+        )
+        os.system(
+            f"scripts\\{etoile_version}\\bin\\EtoileResurrection combine --prefix=lowiro -o arcpkgs\\aprilfools\\combine -s.* -re arcpkgs\\aprilfools\\*.arcpkg fragments-category\\songs\\songlist_aprilfools fragments-category\\songs\\packlist_aprilfools"
         )
     else:
         os.system(
             f"scripts/{etoile_version}/bin/EtoileResurrection pack fragments-category/songs/songlist --songId=.* -re --prefix=lowiro -o arcpkgs"
         )
         os.system(
-            f"scripts/{etoile_version}/bin/EtoileResurrection pack fragments-category/songs/songlist_aprilfools --songId=.* -re --prefix=lowiro -o arcpkgs"
+            f"scripts/{etoile_version}/bin/EtoileResurrection pack fragments-category/songs/songlist_aprilfools --songId=.* -re --prefix=lowiro -o arcpkgs/aprilfools"
+        )
+        os.system(
+            f"scripts/{etoile_version}/bin/EtoileResurrection combine --prefix=lowiro -o arcpkgs/combine -s.* -re arcpkgs/*.arcpkg fragments-category/songs/songlist fragments-category/songs/packlist"
+        )
+        os.system(
+            f"scripts\\{etoile_version}/bin/EtoileResurrection combine --prefix=lowiro -o arcpkgs/aprilfools/combine -s.* -re arcpkgs/aprilfools/*.arcpkg fragments-category/songs/songlist_aprilfools fragments-category/songs/packlist_aprilfools"
         )
